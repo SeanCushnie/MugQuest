678dfb09ceec01afea21ec6a7ad13c260d7b1a8a
Kirsty <kirstmcbride@gmail.com>
2023-09-07 16:54:24 +0100

    fourth state populating

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import request from "../Helpers/request";
 
 export default function NewGame({ user, player, setGame }) {
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const game = await request.createGame(user.id, player.id);
+    setGame(game);
     // Use the navigate function to navigate to the game screen
     navigate("/play");
   };
+
+  
 
   return (
     <main>
